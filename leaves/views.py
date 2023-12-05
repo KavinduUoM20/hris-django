ea4245197d58262ab912c7004657458b1679345a
manumi206144U <wijesinghewaams.20@uom.lk>
2023-12-05 20:21:43 +0530

    login_required updated in leaves/views.py

--- leaves/views.py
+++ leaves/views.py
@@ -4,3 +4,9 @@ from .models import Leave
 from django.contrib import messages
 from hris.decorators import allowed_users
 
+@login_required(login_url='/authentication/login')
+@allowed_users(allowed_roles=['employee','admin'])
+def index(request):
+    return render(request,'leaves/index.html')
+
+
